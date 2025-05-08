5dffaef5456d6f0ea6233eea0a9ba690494d5fac
kmpery <perrikembali@gmail.com>
2025-05-09 02:36:54 +0800

    up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -137,9 +137,39 @@ const Navbar: React.FC = () => {
                 }`}
               >
                 {darkMode ? (
-                  <BsMoon className='w-4 h-4' />
+                  <svg
+                    className='w-4 h-4'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    {' '}
+                    <g transform='scale(1.1) translate(-1.2, -1.2)'>
+                      <path
+                        fill='currentColor'
+                        d='M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z'
+                      />
+                    </g>
+                  </svg>
                 ) : (
-                  <BsSun className='w-4 h-4' />
+                  <svg
+                    className='w-4 h-4'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <g stroke='currentColor' strokeWidth='2'>
+                      <circle cx='12' cy='12' r='6' fill='currentColor' />
+                      <line x1='12' y1='1' x2='12' y2='3' />
+                      <line x1='12' y1='21' x2='12' y2='23' />
+                      <line x1='4.22' y1='4.22' x2='5.64' y2='5.64' />
+                      <line x1='18.36' y1='18.36' x2='19.78' y2='19.78' />
+                      <line x1='1' y1='12' x2='3' y2='12' />
+                      <line x1='21' y1='12' x2='23' y2='12' />
+                      <line x1='4.22' y1='19.78' x2='5.64' y2='18.36' />
+                      <line x1='18.36' y1='5.64' x2='19.78' y2='4.22' />
+                    </g>
+                  </svg>
                 )}
               </span>
             </button>
