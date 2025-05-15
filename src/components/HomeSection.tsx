044d2c1e4386b8cd9245a8ee4f2121ab42268660
kmpery <perrikembali@gmail.com>
2025-05-15 18:49:49 +0800

    update

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -87,7 +87,7 @@ const HomeSection: React.FC = () => {
             >
               <div className='relative w-64 h-64 mx-auto mb-6'>
                 <img
-                  src='https://ibb.co.com/BHRdrg81'
+                  src='https://ibb.co/album/nqtLtT'
                   alt='Groom'
                   className='w-64 h-64 rounded-full object-cover relative z-0'
                 />
