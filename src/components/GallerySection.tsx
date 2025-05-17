19c86fc15d0c0839da6f3f0e48b3cb06d14f7629
kmpery <perrikembali@gmail.com>
2025-05-18 01:27:26 +0800

    update img

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -377,7 +377,7 @@ const GallerySection: React.FC = () => {
             <button
               key={idx}
               onClick={() => setCurrentSlide(idx)}
-              className={`w-2 h-2 rounded-full ${
+              className={`w-1 h-1 rounded-full ${
                 currentSlide === idx ? 'bg-white' : 'bg-white/50'
               } transition`}
             />
