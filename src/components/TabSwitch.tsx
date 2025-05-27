35136533e8e221d2cf314e4bcc5c545f6c098a84
kmpery <perrikembali@gmail.com>
2025-05-27 11:58:27 +0800

    update

--- src/components/TabSwitch.tsx
+++ src/components/TabSwitch.tsx
@@ -8,7 +8,7 @@ interface TabSwitchProps {
 
 const TabSwitch: React.FC<TabSwitchProps> = ({ activeTab, onChange }) => {
   return (
-    <div className='relative flex items-center justify-center mx-auto w-64 h-12 bg-amber-100 dark:bg-sky-950 rounded-full p-1 cursor-pointer shadow-md'>
+    <div className='relative flex hidden items-center justify-center mx-auto w-64 h-12 bg-amber-100 dark:bg-sky-950 rounded-full p-1 cursor-pointer shadow-md '>
       {/* Background slider */}
       <motion.div
         className='absolute top-1 bottom-1 w-[calc(50%-4px)] rounded-full bg-amber-900 dark:bg-sky-700'
@@ -32,7 +32,7 @@ const TabSwitch: React.FC<TabSwitchProps> = ({ activeTab, onChange }) => {
           Mempelai Wanita
         </button>
         <button
-          onClick={() => onChange('pria')}
+          onClick={() => onChange('wanita')} //lock
           className={`flex-1 rounded-full flex items-center justify-center text-sm font-medium z-10 transition-colors duration-200
             ${
               activeTab === 'pria'
